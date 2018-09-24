2104bd760ddde61f5bb77c372aae290a2196d9c0
yash <yhdesai99@ymail.com>
2018-09-24 21:26:39 +0530

    added suggestions, still need to review once

--- app/src/paid/java/com/udacity/gradle/builditbigger/JokeActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/JokeActivityFragment.java
@@ -1,15 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 
 /**
